66637b66523fb317c3e8d9b0d23e28b56ebeefd2
Yongyu <yongyu@lawn-128-61-65-245.lawn.gatech.edu>
2023-11-07 22:29:14 -0500

    add comparison of hashing of basic block

--- evaluate.py
+++ evaluate.py
@@ -3,40 +3,63 @@ import json
 
 dir = 'disasm_hash/'
 
-with open(dir + 'src_hash.json', 'r') as f:
+with open(dir + 'src-t_hash.json', 'r') as f:
     src_j = json.load(f)
 
-with open(dir + 'fw_hash.json', 'r') as f:
+with open(dir + 'build_hash.json', 'r') as f:
     fw_j = json.load(f)
 
+
+def create_select_list(num):
+    select_list = []
+    for i in range(num):
+        select_list.append(
+            {
+                'func': 'test',
+                'diff': 1000,
+                'bb_hash': []
+            }
+        )
+    return select_list
+
+def get_max_diff_sel(select_list):
+    diff_max_tmp = 0
+    idx_tmp = -1
+    for i in range(len(select_list)):
+        if select_list[i]['diff'] > diff_max_tmp:
+            diff_max_tmp = select_list[i]['diff']
+            idx_tmp = i
+    return idx_tmp, diff_max_tmp
+
+
 for key, value in src_j.items():
-    print(key, len(value['bb_hash']))
-    select = {
-        'func': 'test',
-        'diff': 1000,
-        'bb_hash': []
-    }
+    print(key)
+    list_len = 25
+    select_list = create_select_list(list_len)
+    
     for key_can, value_can in fw_j.items():
-        # if value_can['size'] < value['size'] + 10 and value_can['size'] > value['size'] - 10:
+
+        idx_sel, diff_sel = get_max_diff_sel(select_list)
+
         diff_tmp = diff(value['func_hash'], value_can['func_hash'])
-
-        if key_can == key:
-            cnt = 0
-            for bb_hash in value['bb_hash']:
-                for bb_hash_can in value_can['bb_hash']:
-                    if bb_hash == bb_hash_can:
-                        cnt += 1
-            print(key_can, cnt, diff_tmp)
         
-        if diff_tmp < select['diff']:
-            select['func'] = key_can
-            select['diff'] = diff_tmp
-            select['bb_hash'] = value_can['bb_hash']
+        if diff_tmp < diff_sel:
+            select_list[idx_sel]['func'] = key_can
+            select_list[idx_sel]['diff'] = diff_tmp
+            select_list[idx_sel]['bb_hash'] = value_can['bb_hash']
 
-    cnt = 0
-    for bb_hash in value['bb_hash']:
-        for bb_hash_can in select['bb_hash']:
-            if bb_hash == bb_hash_can:
-                cnt += 1
-
-    print(select['func'], cnt, select['diff'])
+    max_sim_bb = 0
+    select = 'none'
+    for select_item in select_list:
+        sim_bb = 0
+        for bb_hash in value['bb_hash']:
+            for bb_hash_sel in select_item['bb_hash']:
+                if bb_hash == bb_hash_sel:
+                    sim_bb += 1
+                    break
+        if sim_bb > max_sim_bb:
+            max_sim_bb = sim_bb
+            select = select_item['func']
+        if select_item['func'] == key:
+            print(select_item['func'], sim_bb)
+    print(select, max_sim_bb)
