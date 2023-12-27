d8b7055f1777c7050b7a7cea28afd0713d8a60d5
Yongyu <xieyongyu16@foxmail.com>
2023-12-27 17:03:09 -0500

    modify extract and hash for PD

---
+++ IDA/extract_pd.py
@@ -0,0 +1,74 @@
+from idautils import *
+from idaapi import *
+import csv
+import json
+
+
+def get_func_for_build():
+    func_name = set()
+    with open('func_list.csv', 'r') as f:
+        r = csv.reader(f, delimiter=',')
+        for row in r:
+            func_name.add(row[-1])
+    return func_name
+
+def dump_function_details(ea):
+    disasm = dict()
+    cnt = 0
+
+    for bb in FlowChart(get_func(ea), flags=FC_PREDS):
+        if bb.start_ea != bb.end_ea:
+            bb_disasm = []
+            for head in Heads(bb.start_ea, bb.end_ea):
+                bb_disasm.append(GetDisasm(head))
+            disasm[bb.start_ea] = bb_disasm
+            cnt += 1
+
+    if cnt > 5:
+        preds_list = []
+        if bb.preds():
+            for preds_bb in bb.preds():
+                preds_list.append(preds_bb.start_ea)
+            print(preds_list)
+        disasm['preds'] = preds_list
+        succs_list = []
+        if bb.succs():
+            for succs_bb in bb.succs():
+                succs_list.append(succs_bb.start_ea)
+            print(succs_list)
+        disasm['succs'] = succs_list
+        return disasm
+    else:
+        return None
+
+
+file_name = get_root_filename()
+
+procname = get_inf_structure().procname.lower()
+disasm_dic = {'arch': procname}
+
+if 'build' in file_name:
+    func_name = get_func_for_build()
+    for ea in Functions():
+        name = get_func_name(ea)
+        if name in func_name:
+            disasm = dump_function_details(ea)
+            if disasm:
+                disasm_dic[name] = disasm
+                print(name, 'done')
+            else:
+                print('Skip', name, 'less than five basic blocks')
+else:
+    func_cnt = 0
+    for ea in Functions():
+        name = get_func_name(ea)
+        disasm = dump_function_details(ea)
+        if disasm:
+            disasm_dic[name] = disasm
+            func_cnt += 1
+    disasm_dic['num'] = func_cnt
+    print(func_cnt, 'done')
+
+disasm_json = json.dumps(disasm_dic)
+with open('../output/' + file_name + '_disasm.json', 'w') as f:
+    f.write(disasm_json)
---
+++ parse_hash_pd.py
@@ -0,0 +1,144 @@
+import os
+import json
+from tlsh import hash
+from hashlib import md5
+
+pkg_name = 'expat'
+
+dir_raw = 'disasm_raw/' + pkg_name +'/'
+dir_norm = 'disasm_hash/' + pkg_name + '/'
+
+def sanitize_arm(disasm_dic):
+    hash_dic = {}
+
+    for key, value in disasm_dic.items():
+        disasm_norm = ''
+        bb_hash = []
+
+        for bb_disasm in value:
+            bb_disasm_norm = []
+
+            for ins_disasm in bb_disasm:                              # type: str
+                if ';' in ins_disasm:
+                    ins_disasm = ins_disasm[:ins_disasm.index(';')]
+
+                ins_disasm = ins_disasm.replace(',', '').split()      # type: list
+                
+                for op in ins_disasm:
+                    if op.startswith('#'):
+                        ins_disasm[ins_disasm.index(op)] = 'imm'
+                    elif op.startswith('loc'):
+                        ins_disasm[ins_disasm.index(op)] = 'addr'
+                    elif op.startswith('0x'):
+                        ins_disasm[ins_disasm.index(op)] = 'addr'
+                    elif '.W' in op:
+                        ins_disasm[ins_disasm.index(op)] = op[:op.index('.W')]
+                    elif '#' in op:
+                        if ']' in op[op.index('#'):]:
+                            op_new = op[:op.index('#')] + 'offs]'
+                        else:
+                            op_new = op[:op.index('#')] + 'offs'
+                        ins_disasm[ins_disasm.index(op)] = op_new
+                    elif '_' in op:
+                        ins_disasm[ins_disasm.index(op)] = 'func'
+                    elif op.startswith('=(a'):
+                        ins_disasm[ins_disasm.index(op)] = 'func'
+
+                bb_disasm_norm += ins_disasm + ['\n']                 # type: list
+
+            bb_disasm_norm = ''.join(op for op in bb_disasm_norm)     # type: str
+            # print(bb_disasm_norm)
+            bb_hash.append(md5(bb_disasm_norm.encode()).hexdigest())
+
+            disasm_norm += bb_disasm_norm                             # type: str
+            
+        hash_dic[key] = {
+            'func_hash': hash(disasm_norm.encode()),
+            'bb_hash': bb_hash 
+        }
+
+    return hash_dic
+
+
+def sanitize_x86(disasm_dic):
+    hash_dic = {}
+
+    # for each function
+    for key_func, value_func in disasm_dic.items():
+        disasm_norm = ''
+        bb_hash = dict()
+        # for each basic block
+        for key, value in value_func.items():
+            if key == 'preds':
+                preds = value
+            elif key == 'succs':
+                succs = value
+            else:
+                bb_disasm_norm = []
+                for ins_disasm in value:                              # type: str
+                    if ';' in ins_disasm:
+                        ins_disasm = ins_disasm[:ins_disasm.index(';')]
+
+                    ins_disasm = ins_disasm.replace(',', '').split()      # type: list
+                    
+                    for op in ins_disasm:
+                        if op.endswith('h') and len(op)!=4:
+                            ins_disasm[ins_disasm.index(op)] = 'imm'
+                        elif len(op)==1 and op.isnumeric():
+                            ins_disasm[ins_disasm.index(op)] = 'imm'
+                        elif op.startswith('loc'):
+                            ins_disasm[ins_disasm.index(op)] = 'addr'
+                        elif '+' in op and ']' in op:
+                            op_new = op[:op.index('+')] + '+imm]'
+                            ins_disasm[ins_disasm.index(op)] = op_new
+                        elif '-' in op and ']' in op:
+                            op_new = op[:op.index('-')] + '-imm]'
+                            ins_disasm[ins_disasm.index(op)] = op_new
+                        elif '_' in op:
+                            ins_disasm[ins_disasm.index(op)] = 'func'
+                        
+                    bb_disasm_norm += ins_disasm + ['\n']                 # type: list
+
+                bb_disasm_norm = ''.join(op for op in bb_disasm_norm)     # type: str
+                # bb_hash.append(md5(bb_disasm_norm.encode()).hexdigest())
+                bb_hash[key] = md5(bb_disasm_norm.encode()).hexdigest()
+
+                disasm_norm += bb_disasm_norm                             # type: str
+                
+        hash_dic[key_func] = {
+            'func_hash': hash(disasm_norm.encode()),
+            'bb_hash': bb_hash,
+            'preds': preds,
+            'succs': succs
+        }
+
+    return hash_dic
+
+
+for file_name in os.listdir(dir_raw):
+
+    hash_dic = {}
+
+    print(file_name)
+
+    with open(dir_raw + file_name, 'r') as f:
+        contents_j = json.load(f)
+
+    arch = contents_j['arch']
+    del contents_j['arch']
+    
+    if 'fw' in file_name:
+        num = contents_j['num']
+        del contents_j['num']
+
+    if arch == 'arm':
+        hash_dic = sanitize_arm(contents_j)
+    else:
+        hash_dic = sanitize_x86(contents_j)
+
+    if 'fw' in file_name:
+        hash_dic['num'] = num
+
+    hash_json = json.dumps(hash_dic)
+    with open(dir_norm + file_name.split('_')[0] + '_hash.json', 'w') as f:
+        f.write(hash_json)
