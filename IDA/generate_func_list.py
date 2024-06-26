2e9bbd10c9560bbac62c4fa86ec269eace526678
Yongyu <xieyongyu16@foxmail.com>
2024-06-26 10:39:45 -0400

    unify db record format

--- IDA/generate_func_list.py
+++ IDA/generate_func_list.py
@@ -2,8 +2,9 @@ from pymongo import MongoClient
 import csv
 from packaging import version
 import sys
-sys.path.append('/media/yongyu/Data/ICS/FSS')
+sys.path.append('/media/yongyu/Data/ICS/ICSBoM')
 from util import config
+import os
 
 client = MongoClient('localhost', 27017)
 db = client['lib_vul_db']
@@ -24,29 +25,22 @@ def calculate_statistics(lib):
     print(len(fix_ver), fix_ver)
     print(len(func_name), func_name)
 
-# per package version (list)
-def generate_func_list_for_list_per_package(lib, lib_ver, fw_ver):
-    if lib == 'openssl':
-        collection = db[lib + '_' + lib_ver[:-1]]
-    else:
-        collection = db[lib]
+
+def generate_func_list_for_rest(lib, lib_ver, fw_ver):
+    collection = db[lib]
     build_version = {lib_ver}
-    # for different openssl series
-    with open('func_list_' + ven + '/' + lib + '_' + fw_ver + '_func_list.csv', 'w') as f:
+
+    dir_ven = 'func_list_' + ven + '/'
+    if not os.path.isdir(dir_ven):
+        os.makedirs(dir_ven)
+    with open(dir_ven + lib + '_' + fw_ver + '_func_list.csv', 'w') as f:
         wr = csv.writer(f)
         for doc in collection.find():
-            if 'affected_since_version' in doc.keys():
-                if lib != 'openssl' and version.parse(lib_ver) < version.parse(doc['affected_since_version']):
-                    print('skip', doc['CVE'], '(affecting since', doc['affected_since_version'], ')')
-                    continue
-                elif lib == 'openssl' and lib_ver < doc['affected_since_version']:
-                    print('skip', doc['CVE'], '(affecting since', doc['affected_since_version'], ')')
-                    continue
-
-            if lib != 'openssl' and version.parse(lib_ver) >= version.parse(doc['fixed_version']):
-                print('skip', doc['CVE'], '(fixed in', doc['fixed_version'], ')')
+            if 'affected_since_version' in doc.keys() and version.parse(lib_ver) < version.parse(doc['affected_since_version']):
+                print('skip', doc['CVE'], '(affecting since', doc['affected_since_version'], ')')
                 continue
-            elif lib == 'openssl' and lib_ver >= doc['fixed_version']:
+
+            if doc['fixed_version'] != 'master' and version.parse(lib_ver) >= version.parse(doc['fixed_version']):
                 print('skip', doc['CVE'], '(fixed in', doc['fixed_version'], ')')
                 continue
             
@@ -66,68 +60,88 @@ def generate_func_list_for_list_per_package(lib, lib_ver, fw_ver):
                         wr.writerow([doc['CVE'], doc['fixed_version'], func])
                 if cnt > 0:
                     build_version.add(doc['fixed_version'])
+
     print(lib, list(build_version))
 
-# per package version (string)
-def generate_func_list_for_string_per_package(lib, lib_ver, fw_ver):
-    collection = db[lib]
+
+def generate_func_list_for_openssl(lib, lib_ver, fw_ver):
+    if lib_ver[0] == '1':
+        collection = db[lib + '_' + lib_ver[:-1]]
+    elif lib_ver[0] == '3':
+        collection = db[lib + '_' + lib_ver[:-2]]
     build_version = {lib_ver}
-    with open('func_list_' + ven + '/' + lib + '_' + fw_ver + '_func_list.csv', 'w') as f:
+
+    dir_ven = 'func_list_' + ven + '/'
+    if not os.path.isdir(dir_ven):
+        os.makedirs(dir_ven)
+    with open(dir_ven + lib + '_' + fw_ver + '_func_list.csv', 'w') as f:
         wr = csv.writer(f)
         for doc in collection.find():
             if 'affected_since_version' in doc.keys():
-                if version.parse(lib_ver) < version.parse(doc['affected_since_version']):
+                if lib_ver[0] == '3' and version.parse(lib_ver) < version.parse(doc['affected_since_version']):
                     print('skip', doc['CVE'], '(affecting since', doc['affected_since_version'], ')')
                     continue
-            if version.parse(lib_ver) >= version.parse(doc['fixed_version']):
+                elif lib_ver[0] == '1' and lib_ver < doc['affected_since_version']:
+                    print('skip', doc['CVE'], '(affecting since', doc['affected_since_version'], ')')
+                    continue
+
+            if lib_ver[0] == '3' and version.parse(lib_ver) >= version.parse(doc['fixed_version']):
                 print('skip', doc['CVE'], '(fixed in', doc['fixed_version'], ')')
                 continue
+            elif lib_ver[0] == '1' and lib_ver >= doc['fixed_version']:
+                print('skip', doc['CVE'], '(fixed in', doc['fixed_version'], ')')
+                continue
+            
             if 'function_name' not in doc.keys():
                 print('error', doc['CVE'], 'has no function name')
             else:
-                if 'update_function_name' in doc.keys():
-                    funcs = doc['update_function_name'][1:-1].split(', ')
-                else:
-                    funcs = doc['function_name'][1:-1].split(', ')
                 cnt = 0
-                for func in funcs:
-                    cnt += 1
-                    print(doc['CVE'], doc['fixed_version'], func)
-                    wr.writerow([doc['CVE'], doc['fixed_version'], func[1:-1]])
+                if 'update_function_name' in doc.keys():
+                    for update_func in doc['update_function_name']:
+                        cnt += 1
+                        print(doc['CVE'], doc['fixed_version'], update_func)
+                        wr.writerow([doc['CVE'], doc['fixed_version'], update_func])
+                else:
+                    for func in doc['function_name']:
+                        cnt += 1
+                        print(doc['CVE'], doc['fixed_version'], func)
+                        wr.writerow([doc['CVE'], doc['fixed_version'], func])
                 if cnt > 0:
                     build_version.add(doc['fixed_version'])
+
     print(lib, list(build_version))
 
-lib_format_dic = {
-    'curl': 'list',
-    'dbus': 'list',
-    'dnsmasq': 'list',
-    'e2fsprogs': 'list',
-    'expat': 'string',
-    'libarchive': 'string',
-    'libgcrypt': 'string',
-    'libmodbus': 'list',
-    'libpcap': 'list',
-    'libssh2': 'list',
-    'libtirpc': 'list',
-    'libxml2': 'string',
-    'mosquitto': 'list',
-    'ncurses': 'list',
-    'openssh': 'string',
-    'openssl': 'list',
-    'perl': 'list',
-    'util-linux': 'list',
-    'zlib': 'list'
-}
+
+lib_list = [
+    'curl',
+    'dbus',
+    'dnsmasq',
+    'e2fsprogs',
+    'expat',
+    'libarchive',
+    'libgcrypt',
+    'libmodbus',
+    'libpcap',
+    'libssh2',
+    'libtirpc',
+    'libxml2',
+    'mosquitto',
+    'ncurses',
+    'openssh',
+    'openssl',
+    'perl',
+    'util-linux',
+    'zlib'
+]
 
 ven = config.ven
 fw_ver = config.fw_ver
 lib = config.lib
 lib_ver = config.lib_ver
 
-if lib_format_dic[lib] == 'list':
-    generate_func_list_for_list_per_package(lib, lib_ver, fw_ver)
-elif lib_format_dic[lib] == 'string':
-    generate_func_list_for_string_per_package(lib, lib_ver, fw_ver)
+if lib == 'openssl':
+    generate_func_list_for_openssl(lib, lib_ver, fw_ver)
+elif lib in lib_list:
+    generate_func_list_for_rest(lib, lib_ver, fw_ver)
 else:
-    print('no package', lib)
+    print('no', lib, 'found in CVE database')
